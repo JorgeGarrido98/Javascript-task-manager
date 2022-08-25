012308c294f2944becd31683555beebdc483a333
jorgecod123 <jorgecod123@gmail.com>
2022-08-25 17:03:20 +0200

    first commit

---
+++ javascript-task-manager-main/src/main.js
@@ -0,0 +1,44 @@
+// MOCK
+const TASKS = [{
+        description: 'Mi primera tarea',
+        done: false,
+    },
+    {
+        description: 'Una tarea sin terminar',
+        done: false,
+    },
+    {
+        description: 'Una tarea terminada',
+        done: true,
+    },
+    {
+        description: 'Una tarea para editar',
+        done: false,
+    },
+    {
+        description: 'Una tarea para eliminar',
+        done: true,
+    },
+    {
+        description: 'Una tarea con un texto muy largo: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid asperiores consequuntur dolorem excepturi fugiat harum ipsa iure laboriosam libero minima neque porro possimus quam, quasi qui saepe velit veritatis."',
+        done: false,
+    },
+    {
+        description: 'Tarea numero 7',
+        done: false,
+    },
+    {
+        description: 'Tarea numero 8',
+        done: false,
+    },
+    {
+        description: 'Tarea numero 9',
+        done: false,
+    },
+    {
+        description: 'Tarea numero 10',
+        done: false,
+    },
+];
+
+// ELEMENTS
